9b056b32b59aa01f5c427ccc2aa137b201a1f7fc
simenhu <simen@primecut.no>
2018-02-26 10:30:05 +0100

    Implemented all the major functions. - set_speed - step_num_steps - stop

--- radarrigg.py
+++ radarrigg.py
@@ -9,6 +9,9 @@
 # -calculate endpoint with switch
 
 import RPi.GPIO as GPIO
+import threading
+import serial
+from time import sleep
 
 class steppermotor():
     """
@@ -16,12 +19,74 @@ class steppermotor():
     jump a number of steps, set speed and made observable by a listener to get
     its position.
     """
-    def __init__(motor_pin):
+    def __init__(motor_pin, dir_pin):
         self.motor_pin = motor_pin
+        self.position = 0
+        self.dir_pin = dir_pin
+        self.stop = False
+        self.ser = serial.Serial(
+               port='/dev/ttyUSB0',
+               baudrate = 9600,
+               parity=serial.PARITY_NONE,
+               stopbits=serial.STOPBITS_ONE,
+               bytesize=serial.EIGHTBITS,
+               timeout=1
+           )
+
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setup([self.motor_pin, self.dir_pin], GPIO.OUT)
 
 
-    def set_speed():
-        pass
+    def set_speed(speed):
+        """
+        This method is called to set a contignous speed on the motor
+        """
+        if speed >= 0:
+            GPIO.output(self.dir_pin, GPIO.HIGH)
+        else:
+            GPIO.output(self.dir_pit, GPIO.LOW)
+        t = threading.Thread(target=__speed__, args=(speed,))
+        t.start()
 
-    def step_num_steps():
-        pass
+    def stop():
+        self.stop = True
+
+    def __speed__(speed):
+        """
+        Helper method to run a contignous speed
+        """
+        while True:
+            if self.stop == True:
+                self.stop = False
+                break
+            GPIO.output(self.motor_pin, GPIO.HIGH)
+            sleep(1/speed)
+            GPIO.output(self.motor_pin, GPIO.LOW)
+            sleep(1/speed)
+            __hasMoved__(speed)
+
+    def __hasMoved__(speed):
+        if speed >= 0:
+            self.position += 1
+        else:
+            self.position -= 1
+        #self.ser.write(self.position)
+        print(self.position)
+
+    def step_num_steps(steps, speed):
+        if steps >= 0:
+            GPIO.output(self.dir_pin, GPIO.HIGH)
+            dif = -1
+        else:
+            GPIO.output(self.dir_pin, GPIO.LOW)
+            dif = 1
+        while steps != 0:
+            GPIO.output(self.motor_pin, GPIO.HIGH)
+            sleep(1/speed)
+            GPIO.output(self.motor_pin, GPIO.LOW)
+            sleep(1/speed)
+            __hasMoved__(speed)
+
+
+    def unconnet():
+        GPIO.cleanup()
